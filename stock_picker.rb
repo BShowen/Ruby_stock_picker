5cd66ec07dbbf26ab42cb09f52d9bedd0c62d2da
Bradley Showen <41598254+BShowen@users.noreply.github.com>
2019-02-24 09:07:15 -0500

    changed variable and array names
    To make code more legible and intuitive.

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,22 +1,22 @@
-def stock_picker array
+def stock_picker stock_prices
     profit = nil
-    stocks = []
-    pairs = array.combination(2).to_a
+    best_stocks = []
+    pairs = stock_prices.combination(2).to_a
     pairs.each do |item| 
         current_pair_profit = item[1] - item[0] 
         if profit == nil
             profit = current_pair_profit 
-            stocks.clear
-            stocks << array.find_index(item[0]) << array.find_index(item[1])
+            best_stocks.clear
+            best_stocks << stock_prices.find_index(item[0]) << stock_prices.find_index(item[1])
         elsif profit < current_pair_profit
             profit = current_pair_profit 
-            stocks.clear
-            stocks << array.find_index(item[0]) << array.find_index(item[1])
+            best_stocks.clear
+            best_stocks << stock_prices.find_index(item[0]) << stock_prices.find_index(item[1])
         end
     end
     if profit <= 0
-        return "To minimize your loss you would buy at index #{stocks[0]}, sell at index #{stocks[1]} and take a loss of $#{profit}.00"
+        return "To minimize your loss you would buy at index #{best_stocks[0]}, sell at index #{best_stocks[1]} and take a loss of $#{profit}.00"
     elsif profit > 0
-        return "You would buy at index #{stocks[0]}, sell at index #{stocks[1]} and take a profit of $#{profit}.00"
+        return "You would buy at index #{best_stocks[0]}, sell at index #{best_stocks[1]} and take a profit of $#{profit}.00"
     end
 end
